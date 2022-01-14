5de55d01753cac8c048cf0293fd0169b90152408
Brianzrte <briaan.zar@gmail.com>
2022-01-13 23:21:19 -0300

    desafio finalizado sin errores

--- ejs/index.js
+++ ejs/index.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const rutaApi = require('./routers/app.routers');
 const path = require('path');
-const { engine } = require('express-handlebars');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const  productos  = require('./data/productos');
 
 //engine
-app.engine(
-    "handlebars",
-    engine({
-      extname: "hbs",
-      defaultLayout: "main.hbs",
-      layoutsDir: path.resolve(__dirname, "./views/layouts"),
-      partialsDir: path.resolve(__dirname, "./views/partials"),
-    })
-);
-app.set("view engine", "handlebars");
+
+app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./views"));
 
 //rutas
